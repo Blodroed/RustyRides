ee16542afbfa6b70b65d6deaeffa853480fe3868
Albert Salvesen-Orø <albertsa@uia.no>
2024-04-26 00:49:29 +0200

    Made the jsonParser class

---
+++ include/JsonParser.h
@@ -0,0 +1,14 @@
+//
+// Created by Albert on 26.04.2024.
+//
+
+#ifndef HELLHUB_JSONPARSER_H
+#define HELLHUB_JSONPARSER_H
+
+
+class JsonParser {
+
+};
+
+
+#endif //HELLHUB_JSONPARSER_H
---
+++ src/JsonParser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Albert on 26.04.2024.
+//
+
+#include "../include/JsonParser.h"
