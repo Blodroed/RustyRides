2707a00bda6888c6bbac69ad6c0cb2312e5f9e9c
Albert Salvesen-Orø <albertsa@uia.no>
2024-04-26 15:24:03 +0200

    Constructors made for Jsonparser
    Also started working on some functions that should be useful for the parser

--- include/JsonParser.h
+++ include/JsonParser.h
@@ -8,13 +8,30 @@
 #include "Car.h"
 
 #include <vector>
+#include <fstream>
+
 
 class JsonParser {
 public:
+    // default constructor with filepath to database.json
+    JsonParser(){
+        filepath = "../database/database.json";
+    };
+    ~JsonParser();
+
+    // constructor with custom filepath
+    // remember to close the file after using it
+    explicit JsonParser(const std::string &filepath);
+
+    // set filepath
+    void setFilepath(const std::string &filepath);
+
+    // functions for importing and exporting to json
     void importCarJson(const std::vector<Car> &cars);
+    void exportCarJson(const std::vector<Car> &cars);
 
 private:
-    
+    std::string filepath;
 };
 
 
--- src/JsonParser.cpp
+++ src/JsonParser.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "../include/JsonParser.h"
+
+#include <iostream>
+
+JsonParser::JsonParser(const std::string &filepath) {
+    this->filepath = filepath;
+};
+
+void JsonParser::setFilepath(const std::string &filepath) {
+    this->filepath = filepath;
+};
+
+void JsonParser::importCarJson(const std::vector<Car> &cars) {
+    std::ifstream file(filepath); // filepath should be set on construction of class
+    if (!file.is_open()) {
+        std::cerr << "Error: File not found or failed to open" << std::endl;
+        return;
+    }
+
+}
