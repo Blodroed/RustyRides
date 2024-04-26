5936694ad59954c718bf11f2d8912fd0abf64bc4
Albert Salvesen-Orø <albertsa@uia.no>
2024-04-26 14:35:30 +0200

    Setup for json adding test

--- include/JsonParser.h
+++ include/JsonParser.h
@@ -5,9 +5,16 @@
 #ifndef HELLHUB_JSONPARSER_H
 #define HELLHUB_JSONPARSER_H
 
+#include "Car.h"
+
+#include <vector>
 
 class JsonParser {
+public:
+    void importCarJson(const std::vector<Car> &cars);
 
+private:
+    
 };
 
 
