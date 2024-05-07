90f41681479f7498851a323621387b83c760f3ca
Albert Salvesen-Orø <albertsa@uia.no>
2024-05-07 12:26:40 +0200

    C++ version control
    Cmakelist has a warning and main.cpp currently prints the c++ version

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 // Created by Albert on 17.04.2024.
 //
 
-#include <vector>
 #include "include/Car.h"
 #include "include/CarManager.h"
 
+#include <vector>
+#include <iostream>
+
 int main() {
+    std::cout << "C++ version: " << __cplusplus << std::endl;
     return 0;
 }
