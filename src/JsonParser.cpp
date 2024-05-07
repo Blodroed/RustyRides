88d7d724a7650a3b8cd9c89f663e0a03df5bee7d
Albert Salvesen-Orø <albertsa@uia.no>
2024-05-07 14:29:00 +0200

    Finished importing from jsonfile for car
    When importing it will wipe the car vector and replace with the values from the json file

--- src/JsonParser.cpp
+++ src/JsonParser.cpp
@@ -7,10 +7,11 @@
 #include "../libs/rapidjson/istreamwrapper.h"
 
 #include <iostream>
+#include <vector>
 
 JsonParser::JsonParser(const std::string &filepath) {
     this->filepath = filepath;
-};
+}
 
 JsonParser::~JsonParser() {
     /* Destructor for JsonParser
@@ -19,17 +20,18 @@ JsonParser::~JsonParser() {
     // TODO: close file if open
 };
 
-void JsonParser::setFilepath(const std::string &filepath) {
+void JsonParser::setFilepath(const std::string &newFilepath) {
     /* Set the filepath for the json file
      * the filepath is set upon construction of the class,
      * but with this function you can change it with the filepath in the argument
      */
-    this->filepath = filepath;
+    this->filepath = newFilepath;
 };
 
-void JsonParser::importCarJson(const std::vector<Car> &cars) {
+void JsonParser::importCarJson(std::vector<Car> &cars) {
     /* Importing cars from json file
      * this import method adds the cars to the vector given to the function
+     * WARNING: the vector is cleared before adding new cars
     */
     std::ifstream file(filepath); // filepath should be set on construction of class
     if (!file.is_open()) {
@@ -46,25 +48,17 @@ void JsonParser::importCarJson(const std::vector<Car> &cars) {
     // accessing the cars array directly
     const auto &carsJson = doc["cars"];
 
+    cars.clear(); // clear the vector before adding new cars
+
     // iterating through the cars array and adding it to the vector
     for (const auto &carJson : carsJson.GetArray()) {
-        std::string RegNr = carJson["RegNr"].GetString();
-
-        // check if car already exist in the vector
-        bool carExist = false;
-        for (const auto &car : cars) {
-            if (car.getRegNr() == RegNr) {
-                carExist = true;
-                break;
-            }
-        }
-
-        int year = carJson["year"].GetInt();
-        int price = carJson["price"].GetInt();
-        int kmDriven = carJson["kmDriven"].GetInt();
-        int seats = carJson["seats"].GetInt();
-
+        Car car(carJson["regNr"].GetString(), carJson["color"].GetString(), carJson["model"].GetString(),
+                carJson["carType"].GetString(), carJson["year"].GetInt(),
+                carJson["price"].GetInt(), carJson["kmDriven"].GetInt(),
+                carJson["seats"].GetInt(), carJson["availability"].GetBool());
         cars.push_back(car);
     }
 
+    // closing the file
+    file.close();
 }
