ef70a6f40c1c89c816e296682d1d1f3e35d5d5ab
Albert Salvesen-Orø <albertsa@uia.no>
2024-05-10 22:18:23 +0200

    filepath update

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,6 +1,6 @@
-#include "mainwindow.h"
+#include "../include/mainwindow.h"
 #include "ui_mainwindow.h"
-#include "customer.h"
+#include "../include/customer.h"
 
 MainWindow::MainWindow(QWidget *parent)
         : QMainWindow(parent)
