15a43c613c818c49a713e0174285086fff5415b6
Albert Salvesen-Orø <albertsa@uia.no>
2024-05-19 17:16:07 +0200

    Createlease Finished maybe

--- src/LeaseManager.cpp
+++ src/LeaseManager.cpp
@@ -3,3 +3,8 @@
 //
 
 #include "../include/LeaseManager.h"
+
+void LeaseManager::createLease(Customer &customer, Car &car, QString &startDate, int daysOfLease, JsonParser &jsonParser) {
+    Lease lease(car.getRegNr(), customer.getPersonNr(), startDate, daysOfLease, car.getPrice());
+    jsonParser.addLease(lease);
+}
