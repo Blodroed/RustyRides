074d772fb36c78b287932cbc243259d00a1b2120
Albert Salvesen-Orø <albertsa@uia.no>
2024-05-19 16:41:14 +0200

    vector for leases

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "include/CarManager.h"
 #include "include/Customer.h"
 #include "include/CustomerManager.h"
+#include "include/Lease.h"
+#include "include/LeaseManager.h"
 #include "include/JsonParser.h"
 // tests
 #include "test/testForStuff.h"
@@ -17,6 +19,7 @@ int main(int argc, char *argv[])
     // init vectors
     std::vector<Car> cars;
     std::vector<Customer> customers;
+    std::vector<Lease> leases;
 
     // init customer manager
 
